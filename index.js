56f091cc89a128b76eba58bf609e33a74394562b
rianhasansiam <rianhasan1971@gmail.com>
2025-01-15 00:07:05 +0600

    seceond commit

--- index.js
+++ index.js
@@ -3,12 +3,69 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 const cors = require('cors')
+
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
-
-app.use(cors())
-app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.idf9u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const jwt = require('jsonwebtoken')
+const cookieparser = require('cookie-parser')
+app.use(cookieparser())
+app.use(cors({
+  origin:['http://localhost:5173'],
+  credentials:true
+}))
+
+
+
+
+app.use(express.json())
+
+const logger=(req,res,next)=>{
+console.log("custom middleware"),
+next()
+}
+
+
+
+
+
+
+
+
+
+
+
+const verifyToken=(req,res,next)=>{
+  const token= req?.cookies?.token
+
+  if(!token){
+    return res.status(401).send({message:'token nai'})
+  }
+
+  jwt.verify(token, process.env.JWT_TOKEN, (err, decoded)=>{
+    if(err){
+      return res.status(401).send({message: 'token vul'})
+    }
+    req.user =decoded //email data pabo aikhane(req.user)
+
+    
+    next()
+  })
+ 
+
+    // console.log('veryfiy middleware')
+
+
+}
+
+
+// email verify 
+// if(req.user.email !== req.query.email){
+//   return res.status(403).send({message:'onno mansuh er data neya jabe na'})
+// }
+
+
 
 
 
@@ -25,11 +82,42 @@ const client = new MongoClient(uri, {
 
 
 
+
+
+
+
 async function run() {
   try {
   
     // await client.connect();
     // await client.db("admin").command({ ping: 1 });
+
+
+
+
+
+app.post('/jwt', async(req,res)=>{
+  const user = req.body
+  const token = jwt.sign(user,process.env.JWT_TOKEN,{expiresIn:'1h'})
+  res.cookie( //cookie te set korteci and cors a origin credential true dite hobe
+    'token', token, { 
+      httpOnly:true,
+      secure:false, //true after deploy  process.env.jwt_token === 'production'
+    }
+  ).send({success:true})
+})
+
+app.post('/logout', async(req,res)=>{
+  res.clearCookie('token',{ 
+    httpOnly:true,
+    secure:false, //true after deploy  process.env.jwt_token === 'production'
+  }).send({success: true})
+})
+
+
+
+
+
 
 
     //collections Names
@@ -41,7 +129,13 @@ async function run() {
 
 
     app.get("/", (req, res) => res.send("Welcome to Rian's Server"));
-
+    
+    app.get('/visas-data',verifyToken,async(req,res)=>{
+     const query= visasCollection.find().collation({ locale: 'en', strength: 2 }).sort({ country: 1 });
+     const result = await query.toArray()
+     console.log('ho ai khanei')
+     res.send(result)
+ })
 
     app.post('/visas-data', async(req,res)=>{
         const visa = req.body
@@ -64,11 +158,6 @@ async function run() {
    })
 
 
-   app.get('/visas-data',async(req,res)=>{
-    const query= visasCollection.find().collation({ locale: 'en', strength: 2 }).sort({ country: 1 });
-    const result = await query.toArray()
-    res.send(result)
-})
 
    app.get('/top-countries-visa',async(req,res)=>{
     const query= topContiesVisasCollection.find()
